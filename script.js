e43e49a9af5f0a35956995300643d06b25cdb239
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-15 17:56:19 -0400

    more quotes, updates string variable

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 var data = {
   "quotes": [
-    {"quote": "it's always sunny in philadelphia"},
-    {"quote": "kitten mittens"},
-    {"quote": "rum ham"},
-    {"quote": "The DENNIS System"}
+    {"quote": "It's always sunny in Philadelphia."},
+    {"quote": "Paddy's Pub: home of the original kitten mittens."},
+    {"quote": "Rum ham, no!"},
+    {"quote": "The DENNIS System."},
+    {"quote": "You gotta pay the troll toll, if you wanna get into that boy's soul."}
   ]
 }
 
 var button = $('button');
 var quotesHere = $('#quoteshere');
+var string = '';
 
 button.on("click", function(){
   var number = $('input').val();
+
   console.log(number);
 })
 
 var addSunQuote = function(number){
+  string = '';
   for (var i = 0; i < number; i++){
     var random = Math.floor(Math.random() * data.quotes.length);
-    console.log(data.quotes[random].quote);
-  }
+    string += (data.quotes[random].quote + ' ');
+  } console.log(string);
 }
