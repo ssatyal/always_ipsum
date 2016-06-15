78b4f6863e48a4384330296d2c9d8ce4d843e8a1
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-15 17:44:18 -0400

    function added to randomly generate sunny quotes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var data = {
   "quotes": [
     {"quote": "it's always sunny in philadelphia"},
-    {"quote": "yes, it is"}
+    {"quote": "kitten mittens"},
+    {"quote": "rum ham"},
+    {"quote": "The DENNIS System"}
   ]
 }
 
 var button = $('button');
+var quotesHere = $('#quoteshere');
 
 button.on("click", function(){
   var number = $('input').val();
   console.log(number);
 })
+
+var addSunQuote = function(number){
+  for (var i = 0; i < number; i++){
+    var random = Math.floor(Math.random() * data.quotes.length);
+    console.log(data.quotes[random].quote);
+  }
+}
