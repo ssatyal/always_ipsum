4039c4adf2d65c00d829a59ea4a95d77bcd430fa
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-15 18:09:48 -0400

    more quotes

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ var data = {
     {"quote": "Paddy's Pub: home of the original kitten mittens."},
     {"quote": "Rum ham, no!"},
     {"quote": "The DENNIS System."},
-    {"quote": "You gotta pay the troll toll, if you wanna get into that boy's soul."}
+    {"quote": "You gotta pay the troll toll, if you wanna get into that boy's soul."},
+    {"quote": "Is your cat making TOO MUCH NOISE ALL THE TIME?"},
+    {"quote": "Wildcard, bitches! Yee-haw!"},
+    {"quote": ""},
+    {"quote": "I will eat your babies, bitch!"},
+    {"quote": "Dayman, fighter of the Nightman, champion of the sun, you're a master of karate and friendship for everyone."},
+    {"quote": "I will SMASH your face into a- into a jelly!"},
+    {"quote": "I browned out that evening."},
+    {"quote": "Can I stop you, though? You keep using this word \"jabroni.\" And...it's awesome!"}
+    {"quote": "I got my Magnum condoms; I got my wad of hundreds. I'm ready to plow."}
+    //{"quote": ""},
+    //{"quote": ""},
+    //{"quote": ""},
   ]
 }
 
